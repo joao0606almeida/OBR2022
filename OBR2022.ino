7764b1a2e9ddddb44d2ac6fb0ed2e959e8941356
joao0606almeida <joao0606almeida@dombarreto.g12.br>
2022-04-19 11:20:14 -0300

    Seguidor de linha basico
    TODO: Ajeitar dois pretos, checkar se calibracao esta OK

--- OBR2022.ino
+++ OBR2022.ino
@@ -8,10 +8,14 @@ Nenhuma garantia sobre o funcionamente deste codigo sera oferecida, mesmo em pla
 //definir portas de sensores/pontes com nomes para conveniencia
 #define infra0 A15
 #define infra1 A14
-#define ponteH1 A0
-#define ponteH2 A1
+#define ponteI1 31
+#define ponteI2 33
+#define ponteI3 35
+#define ponteI4 37
+#define ponteENA 2
+#define ponteENB 3
 //definir variaveis globais de calibracao
-int infraWhiteValue;
+int infraWhiteValue[2];
 //TODO: int infraBlackValue;
 
 /*
@@ -19,9 +23,9 @@ Funcao responsavel por calibrar sensores
 */
 void calibrar(){
   delay(1000);
-  int infraWhiteValue[2];
-  infraWhiteValue[0] = analogRead(infra0);
-  infraWhiteValue[1] = analogRead(infra1);
+  //multiplicamos por mil para diminuir as diferencas da imprecisao de sensores
+  infraWhiteValue[0] = analogRead(infra0)*1000;
+  infraWhiteValue[1] = analogRead(infra1)*1000;
 }
 
 /*
@@ -32,13 +36,56 @@ void setup() {
   pinMode(infra0, INPUT);
   pinMode(infra1, INPUT);
   //definir pinos de ponteH como output
-  pinMode(ponteH1, OUTPUT);
-  pinMode(ponteH2, OUTPUT);
+  pinMode(ponteI1, OUTPUT);
+  pinMode(ponteI2, OUTPUT);
+  pinMode(ponteI3, OUTPUT);
+  pinMode(ponteI4, OUTPUT);
+  digitalWrite(ponteI1, LOW);
+  digitalWrite(ponteI2, LOW);
+  digitalWrite(ponteI3, LOW);
+  digitalWrite(ponteI4, LOW);
   //calibrar sensores
   calibrar();
 }
 
+/*
+Funcao de conveniciencia, checka se e branco ou nao
+*/
+bool ehBranco(int sensorInput, int index){
+  if(sensorInput*1000>=infraWhiteValue[index]){
+    return true;
+  }else{
+    return false;
+  }
+}
 
 void loop() {
-  
+  analogWrite(ponteENA, 80);
+  analogWrite(ponteENB, 80);
+  if(ehBranco(analogRead(infra0),0)&&ehBranco(analogRead(infra1),1)){
+      //tudo branco, frente
+      digitalWrite(ponteI1, HIGH);
+      digitalWrite(ponteI2, LOW);
+      digitalWrite(ponteI3, HIGH);
+      digitalWrite(ponteI4, LOW);
+  }else if(ehBranco(analogRead(infra0),0)){
+      //direita preto, direita
+      digitalWrite(ponteI1, LOW);
+      digitalWrite(ponteI2, HIGH);
+      digitalWrite(ponteI3, HIGH);
+      digitalWrite(ponteI4, LOW);
+  }else if(ehBranco(analogRead(infra1),1)){
+      //esquerda preto, esquerda
+      digitalWrite(ponteI1, HIGH);
+      digitalWrite(ponteI2, LOW);
+      digitalWrite(ponteI3, LOW);
+      digitalWrite(ponteI4, HIGH);
+  }else{
+      //TODO: AJEITAR ISSO, NAO FUNCIONA
+      digitalWrite(ponteI1, HIGH);
+      digitalWrite(ponteI2, LOW);
+      digitalWrite(ponteI3, HIGH);
+      digitalWrite(ponteI4, LOW);
+      delay(1000);
+  }
 }
