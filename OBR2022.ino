c365095f01dd7d67b80cf514add55736b8a4d230
joao0606almeida <joao0606almeida@dombarreto.g12.br>
2022-04-19 10:21:55 -0300

    Arquivo Principal criado
    OBR2022.ino (arquivo principal) criado e compilando como esperado com a biblioteca ultrasom incluida

---
+++ OBR2022.ino
@@ -0,0 +1,15 @@
+/*
+Este e o arquivo principal. Seu nome de difere da convencao de "main.x" devido a restircoes do arduino IDE.
+Este codigo deve ser usado com um Arduino ATMEGA 2560. Ele provavelmente nao funcionara em outros.
+Nenhuma garantia sobre o funcionamente deste codigo sera oferecida, mesmo em placa suportadas pelo mesmo.
+*/
+#include "Libraries/Ultrasom/Ultrasom.hpp"
+
+void setup() {
+ 
+}
+
+void loop() {
+
+
+}
