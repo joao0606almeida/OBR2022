45c4cd802a65359342ef6f0f0d80f8b103c84b4b
joao0606almeida <joao0606almeida@dombarreto.g12.br>
2022-04-20 14:54:41 -0300

    Codigo Preliminar
    TODO: Testar e reler

--- Libraries/Ultrasom/Ultrasom.hpp
+++ Libraries/Ultrasom/Ultrasom.hpp
@@ -10,14 +10,50 @@ class UltrassonicSensor{
 private:
   byte EchoPin = 0;
   byte TriggerPin = 0;
+  float isrRead = 0;
+  bool useISR;
+  long isrTimer;
     public:
         UltrassonicSensor(byte Trigger, byte Echo){
             EchoPin = Echo;
             TriggerPin = Trigger;
             pinMode(Trigger, OUTPUT);
             pinMode(EchoPin, INPUT);
+            switch(EchoPin){
+              case 2:
+                useISR=true;
+                break;
+              case 3:
+                useISR=true;
+                break;
+              case 18:
+                useISR=true;
+                break;
+              case 19:
+                useISR=true;
+                break;
+              case 20:
+                useISR=true;
+                break;
+              case 21:
+                useISR=true;
+                break;
+              default:
+                useISR=false;
+                break;
+            }
+            if(useISR){
+              attachInterrupt(digitalPinToInterrupt(EchoPin), isr , RISING);
+            }
         }
-        float read(byte Echo, byte Trigger){
+        UltrassonicSensor(byte Trigger, byte Echo, bool doIsr){
+          EchoPin = Echo;
+          TriggerPin = Trigger;
+          pinMode(Trigger, OUTPUT);
+          pinMode(EchoPin, INPUT);
+          useISR=doIsr;
+        }
+        float measure(byte Echo, byte Trigger){
             long duracao, cm;
             //limpar trigger pin, evite missfires
             digitalWrite(TriggerPin, LOW);
@@ -34,7 +70,22 @@ private:
             return cm;
         }
         float read(){
-            return read(EchoPin, TriggerPin);
+            if(useISR) return isrRead;
+            return measure(EchoPin, TriggerPin);
+        }
+        void isr(){
+          if(isrTimer!=0){
+            long duracao = micros()-isrTimer;
+            isrRead = (duracao/2) / 29.1;
+          }
+          long builtInTimer = micros();
+          digitalWrite(TriggerPin, LOW);
+          while(builtInTimer+5<micros()){}
+          digitalWrite(TriggerPin, HIGH);
+          builtInTimer = micros();
+          while(builtInTimer+10<micros()){}
+          digitalWrite(TriggerPin, LOW);
+          isrTimer=micros();
         }
 };
 
