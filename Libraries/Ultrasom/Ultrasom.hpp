f6c60be5de1b7247ef0850c2d864ad7f834d6573
joao0606almeida <joao0606almeida@dombarreto.g12.br>
2022-04-20 17:29:51 -0300

    Adiconar getEcho, getTrigger e trigger
    Issue #1 abverto por iago

--- Libraries/Ultrasom/Ultrasom.hpp
+++ Libraries/Ultrasom/Ultrasom.hpp
@@ -10,48 +10,12 @@ class UltrassonicSensor{
 private:
   byte EchoPin = 0;
   byte TriggerPin = 0;
-  float isrRead = 0;
-  bool useISR;
-  long isrTimer;
     public:
         UltrassonicSensor(byte Trigger, byte Echo){
             EchoPin = Echo;
             TriggerPin = Trigger;
             pinMode(Trigger, OUTPUT);
             pinMode(EchoPin, INPUT);
-            switch(EchoPin){
-              case 2:
-                useISR=true;
-                break;
-              case 3:
-                useISR=true;
-                break;
-              case 18:
-                useISR=true;
-                break;
-              case 19:
-                useISR=true;
-                break;
-              case 20:
-                useISR=true;
-                break;
-              case 21:
-                useISR=true;
-                break;
-              default:
-                useISR=false;
-                break;
-            }
-            if(useISR){
-              attachInterrupt(digitalPinToInterrupt(EchoPin), isr , RISING);
-            }
-        }
-        UltrassonicSensor(byte Trigger, byte Echo, bool doIsr){
-          EchoPin = Echo;
-          TriggerPin = Trigger;
-          pinMode(Trigger, OUTPUT);
-          pinMode(EchoPin, INPUT);
-          useISR=doIsr;
         }
         float measure(byte Echo, byte Trigger){
             long duracao, cm;
@@ -70,22 +34,17 @@ private:
             return cm;
         }
         float read(){
-            if(useISR) return isrRead;
             return measure(EchoPin, TriggerPin);
         }
-        void isr(){
-          if(isrTimer!=0){
-            long duracao = micros()-isrTimer;
-            isrRead = (duracao/2) / 29.1;
-          }
-          long builtInTimer = micros();
+        byte getEcho() return EchoPin;
+        byte getTrigger() return TriggerPin;
+        void trigger(){
           digitalWrite(TriggerPin, LOW);
-          while(builtInTimer+5<micros()){}
+          delayMicroseconds(5);
+          //trigger
           digitalWrite(TriggerPin, HIGH);
-          builtInTimer = micros();
-          while(builtInTimer+10<micros()){}
+          delayMicroseconds(10);
           digitalWrite(TriggerPin, LOW);
-          isrTimer=micros();
         }
 };
 
